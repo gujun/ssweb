f8f37ddd5d9875723e167f095a2aa9ffc5efc2d4
Jack Gu <gujuncz@163.com>
2020-07-01 22:24:03 +0800

    change title

--- include/ok.php
+++ include/ok.php
@@ -1,7 +1,7 @@
 <?php
 echo "<html>";
 echo "<head>";
-echo "<title>提示</title>";
+echo "<title>Information</title>";
 echo "</head>";
 echo "<body onload=\"alert('操作成功！');\">";
 echo "<h1>操作成功！</h1>";
